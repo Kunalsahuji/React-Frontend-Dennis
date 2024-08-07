b3b86e1e5e826f2818b18349aa877081e993bb84
Kunalsahuji <ksahu0103@gmail.com>
2024-08-08 00:02:55 +0530

    scrollDirection

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,10 +4,11 @@ const Navbar = () => {
     return (
         <div className='py-9 text-white px-12 z-20 w-screen flex itmes-center justify-between fixed'>
             <h3 className='text-xl'>@Code by Dennis</h3>
-            <div className="flex gap-12 items-center"></div>
-            <h3 className='text-xl'>Work</h3>
-            <h3 className='text-xl'>About</h3>
-            <h3 className='text-xl'>Contact</h3>
+            <div className="flex gap-14 items-center">
+                <h3 className='text-xl'>Work</h3>
+                <h3 className='text-xl'>About</h3>
+                <h3 className='text-xl'>Contact</h3>
+            </div>
         </div>
     )
 }
--- src/components/page1/herosection/HeroText.jsx
+++ src/components/page1/herosection/HeroText.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import { motion, useScroll } from 'framer-motion'
+import React, { useEffect, useState } from 'react'
 
 const HeroText = () => {
+    const scrollY = useScroll().scrollY
+    const [scrollDirection, setScrollDirection] = useState(1)
+    useEffect(() => {
+        scrollY.on('change', () => {
+            const currentScrolling = scrollY.get() - scrollY.getPrevious()
+            if (currentScrolling > 0) {
+                setScrollDirection(1)
+            } else {
+                setScrollDirection(0)
+            }
+        })
+    }, [scrollY])
     return (
         <div className='absolute top-[58%]'>
-            <div className='flex'>
-                <h1 className='text-white whitespace-nowrap font-medium text-[14.5vw]'>- Dennis Snellenberg</h1>
-                <h1 className='text-white whitespace-nowrap font-medium text-[14.5vw]'>- Dennis Snellenberg</h1>
-                <h1 className='text-white whitespace-nowrap font-medium text-[14.5vw]'>- Dennis Snellenberg</h1>
-            </div>
+            <motion.div
+                initial={{
+                    x: -1957,
+                }}
+                animate={{
+                    x: scrollDirection > 0 ? -3914 : 0
+                }}
+                transition={{
+                    duration: 10,
+                    repeat: Infinity,
+                    ease: 'linear'
+                }}
+                className='flex'>
+                <h1 className='text-white whitespace-nowrap font-medium text-[15vw]'>- Dennis Snellenberg</h1>
+                <h1 className='text-white whitespace-nowrap font-medium text-[15vw]'>- Dennis Snellenberg</h1>
+                <h1 className='text-white whitespace-nowrap font-medium text-[15vw]'>- Dennis Snellenberg</h1>
+            </motion.div>
         </div>
     )
 }
